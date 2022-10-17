33807557b8973570bc1d71fe3aa4048ea4abf5c7
Serkan Sadulov <92607156+SerkanSadulov@users.noreply.github.com>
2022-10-17 21:31:03 +0300

    Initial commit

---
+++ Chess/Chess.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum piece {
+	none,
+	king,
+	queen,
+	bishop,
+	pawn,
+	knight,
+	rook,
+};
+piece board[8][8];
+void Board() {
+	//Clearing out the board
+	memset(board, 0, sizeof(board));
+	//Setting up the pieces on the board 
+	board[0][0] = rook;
+	board[0][1] = knight;
+	board[0][2] = bishop;
+	board[0][3] = queen;
+	board[0][4] = king;
+	board[0][5] = bishop;
+	board[0][6] = knight;
+	board[0][7] = rook;
+	board[7][0] = rook;
+	board[7][1] = knight;
+	board[7][2] = bishop;
+	board[7][3] = queen;
+	board[7][4] = king;
+	board[7][5] = bishop;
+	board[7][6] = knight;
+	board[7][7] = rook;
+	for (int i = 0; i < 8; i++) {
+		board[1][i] = pawn;
+		board[6][i] = pawn;
+
+	}
+
+}
+
+
+bool validMove(int xFrom, int yFrom, int xTo, int yTo) {
+
+	int xValid = abs(xFrom - xTo);
+	int yValid = abs(yFrom - yTo);
+
+
+	piece peice = board[xFrom][yFrom];
+
+	switch (peice) {
+	case none:
+		break;
+	case king:
+
+		if (xValid < 2 && yValid < 2 && board[xTo][yTo] == none) {
+			peice = board[xTo][yTo];
+			return true;
+		}
+		else return false;
+
+
+		break;
+	case queen:
+
+		break;
+	case bishop:
+
+		break;
+	case pawn:
+
+		break;
+	case knight:
+
+		break;
+	case rook:
+
+
+
+
+
+		break;
+	default:
+		break;
+	}
+}
+
+int main() {
+	string input;
+	Board();
+	cout << "Move: ";	cin >> input;
+	if (input.length() != 5) {
+		cout << "Invalid input";
+	}
+	else {
+		if ((!isdigit(input[0])) && !isdigit(input[3])) {
+			if ((isdigit(input[1])) && isdigit(input[4])) {
+
+				bool result = validMove(input[1] - 49, input[0] - 97, input[4] - 49, input[3] - 97);
+
+				if (result == true) {
+					cout << "Valid move";
+				}
+				else cout << "Invalid move";
+			}
+
+		}
+	}
+
+
+
+}
